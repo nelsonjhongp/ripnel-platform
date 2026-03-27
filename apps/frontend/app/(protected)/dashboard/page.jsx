c6e033f9de00d0d8187dbc417fd0fb68e548c819
Ivan Manrique <jparatupcya@gmail.com>
2026-03-26 22:58:52 -0500

    Versión base de Dashboard

---
+++ apps/frontend/app/(protected)/dashboard/page.jsx
@@ -0,0 +1,7 @@
+export default function DashboardPage() {
+  return (  
+  <div className="flex flex-col items-center justify-center h-screen">
+    <h1 className="text-4xl font-bold mb-4">Dashboard</h1>
+    <iframe title="Ripnel" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNDgxYzkzYjYtMDQ2OS00ZDBlLWEwYWItMWZkNjIxZGE2ZjYwIiwidCI6ImM0YTY2YzM0LTJiYjctNDUxZi04YmUxLWIyYzI2YTQzMDE1OCIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+  </div>);
+}
